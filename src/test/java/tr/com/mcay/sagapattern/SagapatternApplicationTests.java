df793bd59ea15990e1dcf95ef2a9b5473f0b938d
MUSTAFA ÇAY <mcay51@gmail.com>
2024-09-09 01:50:08 +0300

    initial commit

---
+++ src/test/java/tr/com/mcay/sagapattern/SagapatternApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.mcay.sagapattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagapatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
